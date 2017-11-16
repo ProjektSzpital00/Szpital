fcf782e1c7e29265efe086e0ecfd7e6d36404aa9
Patryk1922 <33635133+Patryk1922@users.noreply.github.com>
2017-11-16 17:46:02 +0100

    0.8
    polskie znaki :)

--- Szpital/src/szpital/util/Laczenie.java
+++ Szpital/src/szpital/util/Laczenie.java
@@ -5,80 +5,55 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Laczenie 
-{
-    private static final String driver = "com.mysql.cj.jdbc.Driver";
-    private static final String databaseUrl1 = "jdbc:mysql://80.211.205.68:3306/Szpital2";
-    private static final String user = "wpiszUzytkownik";
-    private static final String password = "wpiszJakiesHaslo";
-    private static Connection dbConnection;
-    private static Statement stmt;
-    
-    private static void getConnection() throws ClassNotFoundException, SQLException
-    {
-        if(dbConnection == null)
-        {    
-            try 
-            {
-                Class.forName(driver);
-                dbConnection = (Connection) DriverManager.getConnection(databaseUrl1+"?useSSL=false", user, password);
-            } 
-            catch (ClassNotFoundException ex) 
-            {
-                throw new ClassNotFoundException("Błąd sterownika MySQL JDBC", ex);
-            } 
-            catch (SQLException exc) 
-            { 
-                throw new SQLException("Nie udalo się nawiązać połączenia", exc);
-            }
-        }
-    }
-    
-    public static void closeConnection()
-    {
-        if(dbConnection != null)
-        {
-            try 
-            {
-                dbConnection.close();
-            } 
-            catch (SQLException ex) 
-            {
-                System.out.println("Blad przy zamykaniu polaczenia!");
-            }
-        }
-    }
-            
-    public static Statement getStatement() throws SQLException, ClassNotFoundException
-    {
-        if(stmt != null)
-        {
-            return stmt;
-        }
-        else
-        {
-            try 
-            {
-                if(dbConnection != null)
-                {
-                    stmt = dbConnection.createStatement();
-                }
-                else
-                {
-                    getConnection();
-                    stmt = dbConnection.createStatement();
-                }   
-            } 
-            catch (ClassNotFoundException ex) 
-            {
-                throw new ClassNotFoundException("Błąd sterownika MySQL JDBC", ex);
-            } 
-            catch (SQLException exc) 
-            { 
-                throw new SQLException("Nie udalo się nawiązać połączenia", exc);
-            }
+public class Laczenie {
+	private static final String driver = "com.mysql.cj.jdbc.Driver";
+	private static final String databaseUrl1 = "jdbc:mysql://80.211.205.68:3306/Szpital2";
+	private static final String user = "szpital";
+	private static final String password = "haslo";
+	private static Connection dbConnection;
+	private static Statement stmt;
 
-            return stmt;
-        }
-    }
+	private static void getConnection() throws ClassNotFoundException, SQLException {
+		if (dbConnection == null) {
+			try {
+				Class.forName(driver);
+				dbConnection = (Connection) DriverManager.getConnection(databaseUrl1 + "?useUnicode=yes&characterEncoding=UTF-8", user, password);
+			} catch (ClassNotFoundException ex) {
+				throw new ClassNotFoundException("Błąd sterownika MySQL JDBC", ex);
+			} catch (SQLException exc) {
+				throw new SQLException("Nie udalo się nawiązać połączenia", exc);
+			}
+		}
+	}
+
+	public static void closeConnection() {
+		if (dbConnection != null) {
+			try {
+				dbConnection.close();
+			} catch (SQLException ex) {
+				System.out.println("Blad przy zamykaniu polaczenia!");
+			}
+		}
+	}
+
+	public static Statement getStatement() throws SQLException, ClassNotFoundException {
+		if (stmt != null) {
+			return stmt;
+		} else {
+			try {
+				if (dbConnection != null) {
+					stmt = dbConnection.createStatement();
+				} else {
+					getConnection();
+					stmt = dbConnection.createStatement();
+				}
+			} catch (ClassNotFoundException ex) {
+				throw new ClassNotFoundException("Błąd sterownika MySQL JDBC", ex);
+			} catch (SQLException exc) {
+				throw new SQLException("Nie udalo się nawiązać połączenia", exc);
+			}
+
+			return stmt;
+		}
+	}
 }
--- Szpital/src/szpital/util/PacjentUtil.java
+++ Szpital/src/szpital/util/PacjentUtil.java
@@ -8,149 +8,110 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import szpital.model.Pacjent;
 
-public class PacjentUtil
-{
-    private static ObservableList<Pacjent> pacjentList = FXCollections.observableArrayList();
-    
-    public static ObservableList<Pacjent> getPacjentList() throws SQLException, ClassNotFoundException
-    { 
-        try 
-        {
-            Statement stmt = Laczenie.getStatement();
-            
-            try 
-            {
-                String query = "SELECT Pacjenci.id, Pacjenci.imie, Pacjenci.nazwisko, Pacjenci.pesel, Lekarze.id, Lekarze.imie, Lekarze.nazwisko, Oddzialy.id, Oddzialy.nazwa, gr_krwi\n" +
-                                "FROM Pacjenci\n" +
-                                "JOIN Lekarze ON Pacjenci.id_lekarza = Lekarze.id\n" +
-                                "JOIN Oddzialy ON Lekarze.id_oddzialu = Oddzialy.id\n" +
-                                "ORDER BY Pacjenci.id";
-                ResultSet rs = stmt.executeQuery(query);
-                while (rs.next())
-                {
-                    Pacjent p = new Pacjent(rs.getInt("Pacjenci.id"), rs.getString("Pacjenci.imie"), rs.getString("Pacjenci.nazwisko"), rs.getString("Pacjenci.pesel"), rs.getInt("Lekarze.id"), (rs.getString("Lekarze.nazwisko")+" "+rs.getString("Lekarze.imie")), rs.getInt("Oddzialy.id"), rs.getString("Oddzialy.nazwa"), rs.getString("gr_krwi"));
-                    pacjentList.add(p);
-                }
-            }
-            catch(SQLException ex)
-            {
-                throw new SQLException("Błąd zapytania", ex);
-            }
-        } 
-        catch (SQLException | ClassNotFoundException ex) 
-        {
-            throw ex;
-        }
-        
-        return pacjentList;
-    }
-    
-    public static void clearPacjentList()
-    {
-        pacjentList.clear();
-    }
-    
-    public static void addPacjent(Statement statement, Pacjent pacjent)
-    {
-    	
-    	try 
-        {
-            Statement stmt = Laczenie.getStatement();
-            
-            try 
-            {
-            	String query = "insert into Pacjenci (imie, nazwisko, pesel, id_lekarza, id_oddzialu, gr_krwi) "+ 
-                		"Values('"+pacjent.getImie().getValue()+"', '"+pacjent.getNazwisko().getValue()+"', '"+ 
-            		pacjent.getPesel().getValue()+"', "+pacjent.getIdLekarza().getValue()+", "+pacjent.getIdOddzialu().getValue()+", '" +pacjent.getGrKrwii().getValue()+"');";
-                stmt.executeUpdate(query);
-            }
-            catch(SQLException ex)
-            {
-                throw new SQLException("Błąd zapytania (add pacjent)", ex);
-            }
-        } 
-        catch (SQLException | ClassNotFoundException ex) 
-        {
-            Utils.alertWyswietl(ex);
+public class PacjentUtil {
+	private static ObservableList<Pacjent> pacjentList = FXCollections.observableArrayList();
+
+	public static ObservableList<Pacjent> getPacjentList() throws SQLException, ClassNotFoundException {
+		try {
+			Statement stmt = Laczenie.getStatement();
+
+			String query = "SELECT Pacjenci.id, Pacjenci.imie, Pacjenci.nazwisko, Pacjenci.pesel, Lekarze.id, Lekarze.imie, Lekarze.nazwisko, Oddzialy.id, Oddzialy.nazwa, gr_krwi\n"
+					+ "FROM Pacjenci\n" + "JOIN Lekarze ON Pacjenci.id_lekarza = Lekarze.id\n"
+					+ "JOIN Oddzialy ON Lekarze.id_oddzialu = Oddzialy.id\n" + "ORDER BY Pacjenci.id";
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				Pacjent p = new Pacjent(rs.getInt("Pacjenci.id"), rs.getString("Pacjenci.imie"),
+						rs.getString("Pacjenci.nazwisko"), rs.getString("Pacjenci.pesel"), rs.getInt("Lekarze.id"),
+						(rs.getString("Lekarze.nazwisko") + " " + rs.getString("Lekarze.imie")),
+						rs.getInt("Oddzialy.id"), rs.getString("Oddzialy.nazwa"), rs.getString("gr_krwi"));
+				pacjentList.add(p);
+			}
+
+		} catch (SQLException ex) {
+			throw new SQLException("BĹ‚Ä…d zapytania", ex);
+		} catch (ClassNotFoundException ex) {
+			throw ex;
+		}
+
+		return pacjentList;
 	}
-    }
-    
-    public static void updatePacjent(Statement statement, Pacjent pacjent)
-    {
-    	try 
-        {
-            Statement stmt = Laczenie.getStatement();
-            
-            try 
-            {
-            	String query = "UPDATE Pacjenci" + 
-            			" SET imie='"+pacjent.getImie().getValue()+"', nazwisko='"+pacjent.getNazwisko().getValue()+"', pesel='"+pacjent.getPesel().getValue()+
-            			"', id_lekarza="+pacjent.getIdLekarza().getValue()+", id_oddzialu="+pacjent.getIdOddzialu().getValue()+", gr_krwi='"+pacjent.getGrKrwii().getValue()+"'" + 
-            			" WHERE id="+pacjent.getIdPacjenta().getValue()+";";
-                stmt.executeUpdate(query);
-            }
-            catch(SQLException ex)
-            {
-                throw new SQLException("Błąd zapytania (update pacjent)", ex);
-            }
-        } 
-        catch (SQLException | ClassNotFoundException ex) 
-        {
-            Utils.alertWyswietl(ex);
+
+	public static void clearPacjentList() {
+		pacjentList.clear();
 	}
-    }
-    
-    public static void deletePacjent(Statement statement, IntegerProperty idPacjenta)
-    {
-    	try 
-        {
-            Statement stmt = Laczenie.getStatement();
-            
-            try 
-            {
-                String query = "delete from Pacjenci where id =" + idPacjenta.getValue();
-                stmt.executeUpdate(query);
-            }
-            catch(SQLException ex)
-            {
-                throw new SQLException("Błąd zapytania (delete pacjent)", ex);
-            }
-        } 
-        catch (SQLException | ClassNotFoundException ex) 
-        {
-            Utils.alertWyswietl(ex);
+
+	public static void addPacjent(Statement statement, Pacjent pacjent) throws SQLException {
+
+		try {
+			Statement stmt = Laczenie.getStatement();
+			//stmt.executeUpdate("SET NAMES 'utf8'");
+			String query = "insert into Pacjenci (imie, nazwisko, pesel, id_lekarza, id_oddzialu, gr_krwi) "
+					+ "Values('" + pacjent.getImie().getValue() + "', '" + pacjent.getNazwisko().getValue() + "', '"
+					+ pacjent.getPesel().getValue() + "', " + pacjent.getIdLekarza().getValue() + ", "
+					+ pacjent.getIdOddzialu().getValue() + ", '" + pacjent.getGrKrwii().getValue() + "');";
+			stmt.executeUpdate(query);
+
+		} catch (SQLException ex) {
+			throw new SQLException("BĹ‚Ä…d zapytania (add pacjent)", ex);
+		} catch (ClassNotFoundException ex) {
+			Utils.alertWyswietl(ex);
+		}
 	}
-    }
-    
-    public static Integer searchPacjentId(Statement statement, String Pesel)
-    {
-        Integer ip = null;
-        
-        try 
-        {
-            Statement stmt = Laczenie.getStatement();
-            
-            try 
-            {
-                String query = "SELECT id FROM Pacjenci WHERE Pesel = "+Pesel;
-                
-                ResultSet rs = stmt.executeQuery(query);
-                
-                if(rs.next())
-                {
-                    ip = rs.getInt("id");
-                }
-            }
-            catch(SQLException ex)
-            {
-                throw new SQLException("Błąd zapytania (search id pacjenta)", ex);
-            }
-        } 
-        catch (SQLException | ClassNotFoundException ex) 
-        {
-            Utils.alertWyswietl(ex);
+
+	public static void updatePacjent(Statement statement, Pacjent pacjent) throws SQLException {
+		try {
+			Statement stmt = Laczenie.getStatement();
+
+			String query = "UPDATE Pacjenci" + " SET imie='" + pacjent.getImie().getValue() + "', nazwisko='"
+					+ pacjent.getNazwisko().getValue() + "', pesel='" + pacjent.getPesel().getValue() + "', id_lekarza="
+					+ pacjent.getIdLekarza().getValue() + ", id_oddzialu=" + pacjent.getIdOddzialu().getValue()
+					+ ", gr_krwi='" + pacjent.getGrKrwii().getValue() + "'" + " WHERE id="
+					+ pacjent.getIdPacjenta().getValue() + ";";
+			stmt.executeUpdate(query);
+
+		} catch (SQLException ex) {
+			throw new SQLException("BĹ‚Ä…d zapytania (update pacjent)", ex);
+		} catch (ClassNotFoundException ex) {
+			Utils.alertWyswietl(ex);
+		}
 	}
-        
-        return ip;
-    }
+
+	public static void deletePacjent(Statement statement, IntegerProperty idPacjenta) {
+		try {
+			Statement stmt = Laczenie.getStatement();
+
+			try {
+				String query = "delete from Pacjenci where id =" + idPacjenta.getValue();
+				stmt.executeUpdate(query);
+			} catch (SQLException ex) {
+				throw new SQLException("BĹ‚Ä…d zapytania (delete pacjent)", ex);
+			}
+		} catch (SQLException | ClassNotFoundException ex) {
+			Utils.alertWyswietl(ex);
+		}
+	}
+
+	public static Integer searchPacjentId(Statement statement, String Pesel) {
+		Integer ip = null;
+
+		try {
+			Statement stmt = Laczenie.getStatement();
+
+			try {
+				String query = "SELECT id FROM Pacjenci WHERE Pesel = " + Pesel;
+
+				ResultSet rs = stmt.executeQuery(query);
+
+				if (rs.next()) {
+					ip = rs.getInt("id");
+				}
+			} catch (SQLException ex) {
+				throw new SQLException("BĹ‚Ä…d zapytania (search id pacjenta)", ex);
+			}
+		} catch (SQLException | ClassNotFoundException ex) {
+			Utils.alertWyswietl(ex);
+		}
+
+		return ip;
+	}
 }
