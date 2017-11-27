a3e34a038a2640bf7f886ff6930f6bd5fe8473e2
Krzysiek777 <33639245+Krzysiek777@users.noreply.github.com>
2017-11-27 17:03:58 +0100

    0.91

--- Szpital/src/szpital/util/LekarzUtil.java
+++ Szpital/src/szpital/util/LekarzUtil.java
@@ -49,32 +49,6 @@ public class LekarzUtil
         return lekarzList;
     }
     
-    public static ObservableList<Pacjent> getPacjentList() throws SQLException, ClassNotFoundException {
-		try {
-			Statement stmt = Laczenie.getStatement();
-
-			String query = "Select Pacjenci.id, Pacjenci.imie, Pacjenci.nazwisko, Pacjenci.pesel,Oddzialy.nazwa,  Pacjenci.gr_krwi from \r\n" + 
-					"Pacjenci join Oddzialy\r\n" + 
-					"on Pacjenci.id_oddzialu=Oddzialy.id " + "ORDER BY Pacjenci.id";
-			ResultSet rs = stmt.executeQuery(query);
-			while (rs.next()) {
-				Pacjent p = new Pacjent(rs.getInt("Pacjenci.id"), rs.getString("Pacjenci.imie"),
-						rs.getString("Pacjenci.nazwisko"), rs.getString("Pacjenci.pesel"), rs.getInt("Lekarze.id"),
-						(rs.getString("Lekarze.nazwisko") + " " + rs.getString("Lekarze.imie")),
-						rs.getInt("Oddzialy.id"), rs.getString("Oddzialy.nazwa"), rs.getString("gr_krwi"));
-				pacjentList.add(p);
-			}
-
-		} catch (SQLException ex) {
-			throw new SQLException("BĹ‚Ä…d zapytania", ex);
-		} catch (ClassNotFoundException ex) {
-			throw ex;
-		}
-
-		return pacjentList;
-	}
-
-    
     public static void clearLekarzList()
     {
         lekarzList.clear();
