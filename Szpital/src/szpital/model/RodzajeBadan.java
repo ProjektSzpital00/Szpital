045a0b95caa762cb9fed9f3e7d3c4bdc9fc8b717
Patryk1922 <33635133+Patryk1922@users.noreply.github.com>
2017-12-11 17:57:33 +0100

    0.27

---
+++ Szpital/src/szpital/model/RodzajeBadan.java
@@ -0,0 +1,29 @@
+package szpital.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class RodzajeBadan {
+	private IntegerProperty id;
+	private StringProperty nazwa;
+	
+	public RodzajeBadan(Integer id, String nazwa){
+		this.id=new SimpleIntegerProperty(id);
+		this.nazwa=new SimpleStringProperty(nazwa);
+	}
+	
+	public IntegerProperty getId() {
+		return id;
+	}
+	public void setId(IntegerProperty id) {
+		this.id = id;
+	}
+	public StringProperty getNazwa() {
+		return nazwa;
+	}
+	public void setNazwa(StringProperty nazwa) {
+		this.nazwa = nazwa;
+	}
+}
