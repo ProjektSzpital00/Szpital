3138c63e81365b01507a5b3e3ea8b6747a471f09
Krzysiek777 <33639245+Krzysiek777@users.noreply.github.com>
2017-12-20 16:25:10 +0100

    0.30

--- Szpital/src/szpital/view/LekarzController.java
+++ Szpital/src/szpital/view/LekarzController.java
@@ -27,7 +27,6 @@ import szpital.util.Utils;
 public class LekarzController 
 {
     private Account account;
-	private Account a1; /* uwaga */
     private LoginController log;
     private ObservableList<Pacjent> pacjentList;
     private ObservableList <Rezerwacja> rezerwacjaList;
